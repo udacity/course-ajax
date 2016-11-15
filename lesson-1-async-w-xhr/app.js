a826aa199fc01aa3cd6f0a77fcd42e8eb912e433
Richard Kalehoff <richardkalehoff@gmail.com>
2016-11-15 10:58:53 -0800

    set up variables and form event listener

---
+++ lesson-1-async-w-xhr/app.js
@@ -0,0 +1,12 @@
+(function () {
+    const form = document.querySelector('#search-form');
+    const searchField = document.querySelector('#search-keyword');
+    let searchedForText;
+    const responseContainer = document.querySelector('#response-container');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        responseContainer.innerHTML = '';
+        searchedForText = searchField.value;
+    });
+})();
