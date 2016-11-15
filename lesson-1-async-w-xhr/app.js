90bcec32ea11c4d149481cecbe00bf0e65dd93e1
Richard Kalehoff <richardkalehoff@gmail.com>
2016-11-15 10:59:58 -0800

    add async request code

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -8,5 +8,61 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        const imgRequest = new XMLHttpRequest();
+        imgRequest.onload = addImage;
+        imgRequest.onerror = function (err) {
+            requestError(err, 'image');
+        };
+        imgRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        imgRequest.setRequestHeader('Authorization', 'Client-ID 462d22cae6dd1d4877bb082c9e9c6502893a9bb7305d4bf8f681d13b56d4abc3');
+        imgRequest.send();
+
+        const articleRequest = new XMLHttpRequest();
+        articleRequest.onload = addArticles;
+        articleRequest.onerror = function (err) {
+            requestError(err, 'articles');
+        };
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=e6a9801dab184d89a4d77b94ff44048c`);
+        articleRequest.send();
     });
+
+    function addImage() {
+        let htmlContent = '';
+        const data = JSON.parse(this.responseText);
+
+        if (data && data.results && data.results[0]) {
+            const firstImage = data.results[0];
+            htmlContent = `<figure>
+                <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`;
+        } else {
+            htmlContent = '<div class="error-no-image">No images available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
+    function addArticles() {
+        let htmlContent = '';
+        const data = JSON.parse(this.responseText);
+
+        if (data.response && data.response.docs && data.response.docs.length > 1) {
+            htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+                    <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                    <p>${article.snippet}</p>
+                </li>`
+            ).join('') + '</ul>';
+        } else {
+            htmlContent = '<div class="error-no-articles">No articles available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+
+    function requestError(e, part) {
+        console.log(e);
+        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning error-${part}">Oh no! There was an error making a request for the ${part}.</p>`);
+    }
 })();
--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -10,5 +10,60 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        $.ajax({
+            url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+            headers: {
+                Authorization: 'Client-ID 462d22cae6dd1d4877bb082c9e9c6502893a9bb7305d4bf8f681d13b56d4abc3'
+            }
+        }).done(addImage)
+        .fail(function (err) {
+            requestError(err, 'image');
+        });
+
+        $.ajax({
+            url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=e6a9801dab184d89a4d77b94ff44048c`
+        }).done(addArticles)
+        .fail(function (err) {
+            requestError(err, 'articles');
+        });
     });
+
+    function addImage(data) {
+        let htmlContent = '';
+
+        if (data && data.results && data.results.length > 1) {
+            const firstImage = data.results[0];
+            htmlContent = `<figure>
+                <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`;
+        } else {
+            htmlContent = '<div class="error-no-image">No images available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
+    function addArticles(data) {
+        let htmlContent = '';
+
+        if (data.response && data.response.docs && data.response.docs.length > 1) {
+            const articles = data.response.docs;
+            htmlContent = '<ul>' + articles.map(article => `<li class="article">
+                    <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                    <p>${article.snippet}</p>
+                </li>`
+            ).join('') + '</ul>';
+        } else {
+            htmlContent = '<div class="error-no-articles">No articles available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+
+    function requestError(e, part) {
+        console.log(e);
+        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning error-${part}">Oh no! There was an error making a request for the ${part}.</p>`);
+    }
 })();
