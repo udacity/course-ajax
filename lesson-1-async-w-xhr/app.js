d202ea51dc353686c7071af4c7897d52f54f7d13
selimfelex <seelimfelex@hotmail.com>
2021-05-14 23:08:00 +0200

    fixed the article not showing

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -17,13 +17,14 @@
         unsplashRequest.onload= addImage;
         unsplashRequest.setRequestHeader('Authorization', 'Client-ID nRQgo4D9Ip9s2k8YmeR39hV2B6-G9kzjyki9NQfvIrg')
         unsplashRequest.send()
-
+        
+        // this function will add image fetched from Asynchronus Http Request to the page withot the need to reload the page 
         function addImage(){
            // debugger;
            let htmlContenet ='';
            //convert rsposnse to java script
            const data = JSON.parse(this.responseText);
-
+            //check if there is a response image or not
            if (data && data.results && data.results[0]){
                const firstImage = data.results[0];
                htmlContenet = `<figure>
@@ -37,12 +38,36 @@
            responseContainer.insertAdjacentHTML('afterbegin', htmlContenet);
         }
 
-        function addArticles () {}
+        
+        // then make another XHR request to get the articles from NYTimes newspaper related to search
             const articleRequest = new XMLHttpRequest();
+            // when the request retrun it calls the addArticle function
             articleRequest.onload = addArticles;
-            articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=W1fdekfDs1A43nsSvHyyZUK368U2TjKQ`);
+            //https://api.nytimes.com/svc/search/v2/articlesearch.json?q=us&api-key=W1fdekfDs1A43nsSvHyyZUK368U2TjKQ
+            console.log(searchedForText);
+            articleRequest.open('GET', `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=W1fdekfDs1A43nsSvHyyZUK368U2TjKQ`);
             articleRequest.send();
+// this function will add the article fitched from NewYourkTimes related to the search field 
+            function addArticles () {
+                let htmlContenet ='';
+                //convert rsposnse to java script
+                const data = JSON.parse(this.responseText);
+                // check if there is a response article 
+                if (data.response && data.response.docs && data.response.docs.length > 1){
+                 // place result articles in an unordered list 
+                    htmlContenet ='<ul>' + data.response.docs.map(article => `<li class="articles">
+                    <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                    <p>${article.snippet}</p>
+                    </li>`).join('' + '</ul');
+                }else {
+                    htmlContenet = '<div class="error-no-article">Nor Articles Available</div>';
+                }
 
+                responseContainer.insertAdjacentHTML('beforeend',htmlContenet);
+
+
+
+            }
 
     });
 })();
